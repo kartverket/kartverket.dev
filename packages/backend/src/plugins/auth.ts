f7b289d44b174ed1896f3f1b0acbe3f39476fd0a
Johannes Bråthen Oma <johannes.oma@kartverket.no>
2024-01-19 15:48:59 +0100

    Add debug log

--- packages/backend/src/plugins/auth.ts
+++ packages/backend/src/plugins/auth.ts
@@ -38,6 +38,7 @@ export default async function createPlugin(
       oauth2Proxy: providers.oauth2Proxy.create({
         signIn: {
           async resolver({result}, ctx) {
+            console.log(result);
             const email = result.getHeader('x-forwarded-user');
             if (!email) {
               throw new Error('Request did not contain a email')
