749fd0be5f88a768241791cfc51d5c2f5689d3fe
Martin Haram Nygård <martin.nygard@kartverket.no>
2024-03-25 12:50:14 +0100

    Remove logging of auth

--- packages/backend/src/plugins/auth.ts
+++ packages/backend/src/plugins/auth.ts
@@ -60,8 +60,6 @@ export default async function createPlugin(
         signIn: {
           async resolver({result}, ctx) {
             const email = result.getHeader('x-auth-request-email')
-            console.log(email)
-            console.log(result)
             if (!email) {
               throw new Error('Request did not contain an email');
             }
