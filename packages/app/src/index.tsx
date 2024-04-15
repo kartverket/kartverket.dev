8219761525dbdbcf52496fdfe2fc861e8f70d533
Eline Henriksen <eline.henriksen@kartverket.no>
2024-04-15 12:51:02 +0200

    Add posthog to backstage

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -2,5 +2,7 @@ import '@backstage/cli/asset-types';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import posthog from 'posthog-js'
 
+posthog.init('phc_5i5QBLfgf4FXS4hJlnkrLsAzQERS8PALDPmF2YVFQsB', { api_host: 'https://eu.posthog.com' })
 ReactDOM.render(<App />, document.getElementById('root'));
