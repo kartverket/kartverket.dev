d23ec9e0a3359879714fe43fd40f4b5019d82b44
Martin Haram Nygård <martin.nygard@kartverket.no>
2024-10-17 13:45:13 +0200

    upgrade to working db tf

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,7 +1,6 @@
 module "cloudsql" {
-  source        = "git@github.com:kartverket/terraform-modules.git/?ref=cloud_sql/v0.2.2"
+  source        = "git@github.com:kartverket/terraform-modules.git/?ref=cloud_sql/v0.2.3"
   instance_name = "backstage"
   env           = var.env
   project_id    = var.gcp_project_id
-  instance_tier = "db-g1-small"
 }
