2586c1accdf234b8068511e872fda7510fa67d36
Martin Haram Nygård <martin.nygard@kartverket.no>
2024-10-22 10:30:43 +0200

    Upgrade database module

--- terraform/database.tf
+++ terraform/database.tf
@@ -1,5 +1,5 @@
 module "cloudsql" {
-  source        = "git@github.com:kartverket/terraform-modules.git/?ref=cloud_sql/v0.2.3"
+  source        = "git@github.com:kartverket/terraform-modules.git/?ref=cloud_sql/v0.3.0"
   instance_name = "backstage"
   env           = var.env
   project_id    = var.gcp_project_id
