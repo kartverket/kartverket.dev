b48dc2afa153b8104b684cb653b33814c6b3c6ac
Emil Telstad <22004178+emilte@users.noreply.github.com>
2025-05-14 16:51:59 +0200

    Upgrade react 18 (#266)
    * .

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,10 +1,13 @@
 import '@backstage/cli/asset-types';
-import ReactDOM from 'react-dom';
 import App from './App';
 import posthog from 'posthog-js';
 import '@backstage/canon/css/styles.css';
+import { createRoot } from 'react-dom/client';
 
 posthog.init('phc_5i5QBLfgf4FXS4hJlnkrLsAzQERS8PALDPmF2YVFQsB', {
   api_host: 'https://eu.posthog.com',
 });
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
