e85c3d23667faa14be2208475db6e5404da04855
Truls Nergaard <truls@anzu.no>
2025-10-13 15:21:53 +0200

    feat: using database call to fetch broken relations

--- packages/backend/src/plugins/extensions/catalogNotificationsModule.ts
+++ packages/backend/src/plugins/extensions/catalogNotificationsModule.ts
@@ -3,9 +3,13 @@ import {
   createBackendModule,
 } from '@backstage/backend-plugin-api';
 import { notificationService } from '@backstage/plugin-notifications-node';
-import { CatalogApi, CatalogClient } from '@backstage/catalog-client';
+import { CatalogClient } from '@backstage/catalog-client';
 
-import { Entity } from '@backstage/catalog-model';
+type MissingRelationTarget = {
+  ref: string;
+  target_ref: string;
+  owner_ref: string | null;
+};
 
 export const catalogNotificationsModule = createBackendModule({
   pluginId: 'catalog',
@@ -17,49 +21,66 @@ export const catalogNotificationsModule = createBackendModule({
         scheduler: coreServices.scheduler,
         discovery: coreServices.discovery,
         auth: coreServices.auth,
+        db: coreServices.database,
         notificationService: notificationService,
       },
       async init({
         logger,
         scheduler,
+        db,
         discovery,
         auth,
         notificationService: notification,
       }) {
-        async function getRelationWarnings(
-          entity: Entity,
-          catalogApi: CatalogApi,
-          token: string,
-        ) {
-          const entityRefRelations = entity.relations?.map(
-            relation => relation.targetRef,
-          );
-          if (
-            !entityRefRelations ||
-            entityRefRelations?.length < 1 ||
-            entityRefRelations.length > 100
-          ) {
-            return [];
-          }
-
-          const relatedEntities = await catalogApi.getEntitiesByRefs(
-            {
-              entityRefs: entityRefRelations,
-              fields: ['kind', 'metadata.name', 'metadata.namespace'],
-            },
-            { token },
-          );
-
-          return entityRefRelations.filter(
-            (_, index) => relatedEntities.items[index] === undefined,
-          );
-        }
+        const database = await db.getClient();
 
         await scheduler.scheduleTask({
           id: 'catalog-error-monitor',
-          frequency: { minutes: 30 },
+          frequency: { seconds: 3 },
           timeout: { minutes: 3 },
           fn: async () => {
+            let query: MissingRelationTarget[];
+            try {
+              query = await database
+                .with('missing', qb => {
+                  qb.select(
+                    'sources.entity_id AS id',
+                    'sources.entity_ref AS ref',
+                    'r.target_entity_ref AS target_ref',
+                  )
+                    .from('relations as r')
+                    .leftJoin(
+                      'refresh_state as targets',
+                      'r.target_entity_ref',
+                      'targets.entity_ref',
+                    )
+                    .join(
+                      'refresh_state as sources',
+                      'r.originating_entity_id',
+                      'sources.entity_id',
+                    )
+                    .whereNotIn('r.type', [
+                      'memberOf',
+                      'hasMember',
+                      'parentOf',
+                      'childOf',
+                    ])
+                    .whereNull('targets.entity_ref');
+                })
+                .select('m.ref', 'm.target_ref', 'o.entity_ref as owner_ref')
+                .from('missing as m')
+                .join('relations as r', 'm.id', 'r.originating_entity_id')
+                .leftJoin(
+                  'refresh_state as o',
+                  'r.target_entity_ref',
+                  'o.entity_ref',
+                )
+                .where('r.type', 'ownedBy');
+            } catch (e) {
+              logger.error(e.toString());
+              return;
+            }
+
             const { token } = await auth.getPluginRequestToken({
               onBehalfOf: await auth.getOwnServiceCredentials(),
               targetPluginId: 'catalog',
@@ -69,69 +90,40 @@ export const catalogNotificationsModule = createBackendModule({
               discoveryApi: discovery,
             });
 
-            try {
-              const { items: entities } = await catalogClient.getEntities(
-                {
-                  filter: [
-                    {
-                      kind: [
-                        'Component',
-                        'Domain',
-                        'System',
-                        'Resource',
-                        'Template',
-                        'Location',
-                        'API',
-                      ],
-                    },
-                  ],
+            const entities = await catalogClient.getEntitiesByRefs(
+              {
+                entityRefs: query.map(({ ref }) => ref),
+                fields: ['kind', 'metadata.name', 'metadata.namespace'],
+              },
+              { token },
+            );
+
+            for (let i = 0; i < entities.items.length; i++) {
+              const entity = entities.items[i];
+              const missingRelation = query[i];
+              if (!entity) throw Error('Missing relation source entity');
+              if (!entity) throw Error('Missing missing relation relation');
+
+              const namespace = entity.metadata.namespace ?? 'default';
+              const entityLink = `/catalog/${namespace}/${entity.kind}/${entity.metadata.name}`;
+              const notificationScope = `${entity.metadata.name}${missingRelation.target_ref}`;
+              const ownerRef =
+                missingRelation.owner_ref ?? 'group:default/skvis';
+
+              notification.send({
+                recipients: {
+                  type: 'entity',
+                  entityRef: ownerRef,
                 },
-                { token },
-              );
-
-              for (const entity of entities) {
-                const relationWarnings = await getRelationWarnings(
-                  entity,
-                  catalogClient,
-                  token,
-                );
-                let entityNamespace = 'default';
-                let entityOwner = '';
-                if (entity.kind === 'Group') {
-                  entityOwner = entity.metadata.name;
-                  entityNamespace = entity.metadata.namespace
-                    ? entity.metadata.namespace
-                    : 'default';
-                  logger.error(`Entityname på group: ${entity.metadata.name}`);
-                } else {
-                  entityOwner = entity.spec?.owner
-                    ? (entity.spec.owner as string)
-                    : 'skvis';
-                }
-
-                const entityLink = `/catalog/${entityNamespace}/${entity.kind}/${entity.metadata.name}`;
-
-                const notificationScope = `${entity.metadata.name}${relationWarnings.join()}`;
-
-                if (relationWarnings.length > 0) {
-                  notification.send({
-                    recipients: {
-                      type: 'entity',
-                      entityRef: `group:default/${entityOwner}`,
-                    },
-                    payload: {
-                      title: `Relation error: ${entity.metadata.name}`,
-                      description: `This entity has relations to other entities, which can't be found in the catalog.
-                                    Entities not found are: ${relationWarnings.join('')}`,
-                      severity: 'high',
-                      link: entityLink,
-                      scope: notificationScope,
-                    },
-                  });
-                }
-              }
-            } catch (error) {
-              logger.error('Failed to check catalog for errors:');
+                payload: {
+                  title: `Relation error: ${entity.metadata.name}`,
+                  description: `This entity has relations to other entities, which can't be found in the catalog.
+                                    Entity not found: ${missingRelation.target_ref}`,
+                  severity: 'high',
+                  link: entityLink,
+                  scope: notificationScope,
+                },
+              });
             }
           },
         });
