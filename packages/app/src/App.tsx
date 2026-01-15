738dbdc2d23bcc892b0c45b71289d589abe2f2b2
Lisa Haukås <70948891+lihauk@users.noreply.github.com>
2026-01-15 10:37:04 +0100

    Oversiktside for funksjoner v1

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -55,6 +55,7 @@ import {
 } from '@kartverket/backstage-plugin-catalog-creator';
 import { NotificationsPage } from '@backstage/plugin-notifications';
 import { SignalsDisplay } from '@backstage/plugin-signals';
+import { FunctionsPage } from './components/functions/FunctionsPage';
 
 const app = createApp({
   __experimentalTranslations: {
@@ -132,10 +133,7 @@ const routes = (
     </Route>
     <Route path="/catalog" element={<CatalogIndexPage key="catalog" />} />
     <FeatureFlagged with="show-functions-page">
-      <Route
-        path="/functions"
-        element={<CatalogIndexPage key="functions" initialKind="function" />}
-      />
+      <Route path="/functions" element={<FunctionsPage />} />
     </FeatureFlagged>
     <Route
       path="/catalog/:namespace/:kind/:name"
