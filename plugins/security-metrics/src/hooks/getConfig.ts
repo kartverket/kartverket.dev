faf41ffebcf6b69d5476d2f2e0d915e3f76421e5
Aleksander Obrestad <aleksander@bekk-mac-3480.local>
2026-01-16 15:45:21 +0100

    Prettier

--- plugins/security-metrics/src/hooks/getConfig.ts
+++ plugins/security-metrics/src/hooks/getConfig.ts
@@ -33,7 +33,7 @@ export const useConfig = (type: MetricTypes): UseConfigReturn => {
 
   const endpointUrl = new URL(
     type === MetricTypes.acceptVulnerability ||
-    type === MetricTypes.configureNotifications
+      type === MetricTypes.configureNotifications
       ? `${backendUrl}/api/security-metrics/proxy/${type}`
       : `${backendUrl}/api/security-metrics/proxy/fetch-${type}`,
   );
