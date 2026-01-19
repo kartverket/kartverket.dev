43735c06f094b9bc4e453e1f066adf570b62989a
gyda <gyda1210@hotmail.com>
2026-01-19 14:55:27 +0100

    text presisering

--- plugins/security-metrics/src/components/SlackNotificationsDialog.tsx
+++ plugins/security-metrics/src/components/SlackNotificationsDialog.tsx
@@ -151,7 +151,7 @@ export const SlackNotificationDialog = ({
         <Box sx={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
           <Typography>
             NB! For å varsle i riktig Slack-kanal må appen “Sårbarhetsvarsler”
-            være lagt til.
+            være lagt til før du lagrer.
           </Typography>
           <Tooltip title="Tips: Gå til kanalen, skriv /add, og legg til appen.">
             <InfoOutlinedIcon
