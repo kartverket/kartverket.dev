5da18e09f441b44f3dce7a751a77a3ac003435ea
Erik Torvbråten <erik.torvbraten@bekk.no>
2026-01-19 07:52:58 +0100

    [Catalog-creator-plugin] Legg til tekst i formet (#609)
    * feat: move info text inside form
    
    * added borders to alerts
    
    ---------
    
    Co-authored-by: Aleksander Obrestad <aleksander@bekk-mac-3480.local>
    Co-authored-by: Håkon Frøland <31542827+hakonfro@users.noreply.github.com>

--- plugins/catalog-creator/src/components/CatalogCreatorPage/RepositoryForm.tsx
+++ plugins/catalog-creator/src/components/CatalogCreatorPage/RepositoryForm.tsx
@@ -1,12 +1,17 @@
-import { TextField, Button, Box, Flex } from '@backstage/ui';
+import { TextField, Button, Box, Flex, Text } from '@backstage/ui';
 import { useTranslationRef } from '@backstage/core-plugin-api/alpha';
 import { catalogCreatorTranslationRef } from '../../utils/translations';
+import style from '../../catalog.module.css';
+import EditDocumentIcon from '@mui/icons-material/EditDocument';
+import Link from '@material-ui/core/Link';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 
 interface RepositoryFormProps {
   url: string;
   onUrlChange: (url: string) => void;
   onSubmit: (e: React.FormEvent) => void;
   disableTextField: boolean;
+  docsLink?: string;
 }
 
 export const RepositoryForm = ({
@@ -14,12 +19,38 @@ export const RepositoryForm = ({
   onUrlChange,
   onSubmit,
   disableTextField,
+  docsLink,
 }: RepositoryFormProps) => {
   const { t } = useTranslationRef(catalogCreatorTranslationRef);
 
   return (
     <form onSubmit={onSubmit}>
-      <Box px="2rem">
+      <Box className={style.infoText}>
+        <Flex align="center" gap="sm">
+          <EditDocumentIcon className={style.icon} />
+          <Text>{t('formInfo.p1')}</Text>
+        </Flex>
+        <Flex direction="column" className={style.learnMoreLinks}>
+          <div>
+            {docsLink && (
+              <div className={style.learnMoreLink}>
+                <Link href={docsLink}>{t('formInfo.linkText')}</Link>
+              </div>
+            )}
+            <div className={style.learnMoreLink}>
+              <Link
+                href="https://backstage.io/docs/features/software-catalog/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                {t('formInfo.linkText2')}
+                <OpenInNewIcon fontSize="inherit" />
+              </Link>
+            </div>
+          </div>
+        </Flex>
+      </Box>
+      <Box>
         <Flex align="end">
           <div style={{ flexGrow: 1 }}>
             <TextField
