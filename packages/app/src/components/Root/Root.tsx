5ce9824b337468bfd59dfdb8ceffc7c027b2c65c
Aleksander Obrestad <aleksander@bekk-mac-3480.local>
2026-01-19 10:32:23 +0100

    Prettier

--- packages/app/src/components/Root/Root.tsx
+++ packages/app/src/components/Root/Root.tsx
@@ -73,83 +73,82 @@ export const Root = ({ children }: PropsWithChildren<{}>) => {
         </SidebarGroup>
         <SidebarItem icon={HomeIcon} to="/" text={t('sidebar.homeTitle')} />
 
-      <SidebarDivider />
-      <SidebarGroup label="mygroup" icon={<MenuIcon />}>
-        {/* Global nav, not org-specific */}
-        <MyGroupsSidebarItem
-          singularTitle="My Group"
-          pluralTitle="My Groups"
-          icon={GroupIcon}
-        />
-        <SidebarItem icon={AppsIcon} to="catalog" text="Catalog" />
-        <FeatureFlagged with="show-functions-page">
-          <SidebarItem icon={TreeIcon} to="functions" text="Functions" />
-        </FeatureFlagged>
-        <SidebarItem
-          icon={EditIcon}
-          to="catalog-creator"
-          text="Edit or Create"
-        />
-        <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
-
-        {/* End global nav */}
         <SidebarDivider />
-        <SidebarGroup label={t('sidebar.myGroupTitle')} icon={<MenuIcon />}>
+        <SidebarGroup label="mygroup" icon={<MenuIcon />}>
           {/* Global nav, not org-specific */}
           <MyGroupsSidebarItem
-            singularTitle={t('sidebar.myGroupTitle')}
+            singularTitle="My Group"
             pluralTitle="My Groups"
             icon={GroupIcon}
           />
+          <SidebarItem icon={AppsIcon} to="catalog" text="Catalog" />
+          <FeatureFlagged with="show-functions-page">
+            <SidebarItem icon={TreeIcon} to="functions" text="Functions" />
+          </FeatureFlagged>
           <SidebarItem
             icon={EditIcon}
             to="catalog-creator"
-            text={t('sidebar.editOrCreateTitle')}
+            text="Edit or Create"
           />
-
-          <SidebarItem
-            icon={EditIcon}
-            to="catalog"
-            text={t('sidebar.catalogTitle')}
-          />
-          <FeatureFlagged with="show-functions-page">
-            <SidebarItem
-              icon={TreeIcon}
-              to="functions"
-              text={t('sidebar.functionTitle')}
-            />
-          </FeatureFlagged>
-          <SidebarItem
-            icon={LibraryBooks}
-            to="docs"
-            text={t('sidebar.docsTitle')}
-          />
+          <SidebarItem icon={LibraryBooks} to="docs" text="Docs" />
 
           {/* End global nav */}
           <SidebarDivider />
-          <SidebarScrollWrapper>
-            <SidebarItem icon={SpeedIcon} to="lighthouse" text="Lighthouse" />
-          </SidebarScrollWrapper>
-          <SidebarItem icon={WarningIcon} to="opencost" text="SKIPcost" />
-        </SidebarGroup>
-        <SidebarSpace />
-        <SidebarDivider />
-        <NotificationsSidebarItem text={t('sidebar.notificationsTitle')} />
-        <SidebarDivider />
-        <SidebarGroup
-          label={t('sidebar.settingsTitle')}
-          icon={<UserSettingsSignInAvatar />}
-          to="/settings"
-        >
-          {/* <SidebarSettings /> */}
-          <SidebarItem
-            icon={SettingsIcon}
-            to="settings"
-            text={t('sidebar.settingsTitle')}
-          />
-        </SidebarGroup>
-                </SidebarGroup>
+          <SidebarGroup label={t('sidebar.myGroupTitle')} icon={<MenuIcon />}>
+            {/* Global nav, not org-specific */}
+            <MyGroupsSidebarItem
+              singularTitle={t('sidebar.myGroupTitle')}
+              pluralTitle="My Groups"
+              icon={GroupIcon}
+            />
+            <SidebarItem
+              icon={EditIcon}
+              to="catalog-creator"
+              text={t('sidebar.editOrCreateTitle')}
+            />
 
+            <SidebarItem
+              icon={EditIcon}
+              to="catalog"
+              text={t('sidebar.catalogTitle')}
+            />
+            <FeatureFlagged with="show-functions-page">
+              <SidebarItem
+                icon={TreeIcon}
+                to="functions"
+                text={t('sidebar.functionTitle')}
+              />
+            </FeatureFlagged>
+            <SidebarItem
+              icon={LibraryBooks}
+              to="docs"
+              text={t('sidebar.docsTitle')}
+            />
+
+            {/* End global nav */}
+            <SidebarDivider />
+            <SidebarScrollWrapper>
+              <SidebarItem icon={SpeedIcon} to="lighthouse" text="Lighthouse" />
+            </SidebarScrollWrapper>
+            <SidebarItem icon={WarningIcon} to="opencost" text="SKIPcost" />
+          </SidebarGroup>
+          <SidebarSpace />
+          <SidebarDivider />
+          <NotificationsSidebarItem text={t('sidebar.notificationsTitle')} />
+          <SidebarDivider />
+          <SidebarGroup
+            label={t('sidebar.settingsTitle')}
+            icon={<UserSettingsSignInAvatar />}
+            to="/settings"
+          >
+            {/* <SidebarSettings /> */}
+            <SidebarItem
+              icon={SettingsIcon}
+              to="settings"
+              text={t('sidebar.settingsTitle')}
+            />
+          </SidebarGroup>
+        </SidebarGroup>
       </Sidebar>
       {children}
     </SidebarPage>
