ca425c85c76bdc8d180f9b93d4ff51b8ad1ce073
Aleksander Tjensvoll Obrestad <aleksanderobre@gmail.com>
2026-01-23 10:43:10 +0100

    added norwegian translations to sidebar and homepage (#621)
    Co-authored-by: Aleksander Obrestad <aleksander@bekk-mac-3480.local>

---
+++ packages/app/src/utils/translations.ts
@@ -0,0 +1,72 @@
+import {
+  createTranslationRef,
+  createTranslationResource,
+} from '@backstage/core-plugin-api/alpha';
+
+export const sidebarMessages = {
+  sidebar: {
+    searchTitle: 'Search',
+    homeTitle: 'Home',
+    myGroupTitle: 'My group',
+    myGroupsTitle: 'My groups',
+    editOrCreateTitle: 'Edit or Create',
+    catalogTitle: 'Catalog',
+    functionsTitle: 'Functions',
+    docsTitle: 'Docs',
+    notificationsTitle: 'Notifications',
+    settingsTitle: 'Settings',
+  },
+};
+
+export const sidebarTranslationRef = createTranslationRef({
+  id: 'sidebar',
+  messages: sidebarMessages,
+});
+
+export const sidebarNorwegianTranslation = createTranslationResource({
+  ref: sidebarTranslationRef,
+  translations: {
+    no: () =>
+      Promise.resolve({
+        default: {
+          'sidebar.searchTitle': 'Søk',
+          'sidebar.homeTitle': 'Hjem',
+          'sidebar.myGroupTitle': 'Min gruppe',
+          'sidebar.myGroupsTitle': 'Mine grupper',
+          'sidebar.editOrCreateTitle': 'Rediger/opprett',
+          'sidebar.catalogTitle': 'Katalog',
+          'sidebar.functionsTitle': 'Funksjoner',
+          'sidebar.docsTitle': 'Dokumentasjon',
+          'sidebar.notificationsTitle': 'Varsler',
+          'sidebar.settingsTitle': 'Innstillinger',
+        },
+      }),
+  },
+});
+
+export const homepageMessages = {
+  homepage: {
+    recentlyVisited: 'Recently Visited',
+    favorites: 'Favorites',
+    toolkit: 'Toolkit',
+  },
+};
+
+export const homepageTranslationRef = createTranslationRef({
+  id: 'homepage',
+  messages: homepageMessages,
+});
+
+export const homepageNorwegianTranslation = createTranslationResource({
+  ref: homepageTranslationRef,
+  translations: {
+    no: () =>
+      Promise.resolve({
+        default: {
+          'homepage.recentlyVisited': 'Nylig besøkt',
+          'homepage.favorites': 'Favoritter',
+          'homepage.toolkit': 'Verktøysett',
+        },
+      }),
+  },
+});
