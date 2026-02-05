470f328bb1860277a11cc339820b40e0ae65919c
aleksanderobrestad <aleksanderobre@gmail.com>
2026-02-05 13:43:01 +0100

    removed unwanted formtypes

--- plugins/frontend-custom-components/src/components/FunctionLinksCard/FunctionLinksCard.tsx
+++ plugins/frontend-custom-components/src/components/FunctionLinksCard/FunctionLinksCard.tsx
@@ -42,8 +42,6 @@ const queryClient = new QueryClient();
 
 const FORM_TYPE_MAP: Record<string, string> = {
   '816cc808-9188-44a9-8f4b-5642fc2932c4': 'Tjenestenivå og driftskontinuitet',
-  '248f16c3-9c0e-4177-bf57-aa7d10d2671c': 'IP og DPIA (BETA – UNDER ARBEID)',
-  '570e9285-3228-4396-b82b-e9752e23cd73': 'Sikkerhetskontroller',
   'e3ab7a6c-c54e-4240-8314-45990e1d7cf1': 'Datasettvurdering',
 };
 
@@ -56,7 +54,7 @@ export const FunctionLinksCard = () => {
 };
 
 function FunctionLinksCardItem(props: EntityLinksCardProps) {
-  const { cols = undefined, variant } = props;
+  const { cols = 1, variant } = props;
   const config = useApi(configApiRef);
   const catalogApi = useApi(catalogApiRef);
   const { entity } = useEntity();
