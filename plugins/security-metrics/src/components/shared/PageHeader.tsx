ea878bf02a11c540e9d7c158cf22da92e7b6f51f
SeverinKV <Severin.Erstad@kartverket.no>
2026-09-11 08:11:06 +0200

    Remove alert banner from sikkerhetsmetrikker (#898)
    * Remove alert banner from sikkerhetsmetrikker
    
    * Formatting
    
    * Formatting again
    
    * Fix missing newline at end of PageHeader.tsx
    
    * Testing own permits
    
    ---------
    
    Co-authored-by: Severin Ullavik Erstad <erssev@PB14250-ERS.statkart.no>
    Co-authored-by: Mikkel <mikkel.gjestrud@kartverket.no>

--- plugins/security-metrics/src/components/shared/PageHeader.tsx
+++ plugins/security-metrics/src/components/shared/PageHeader.tsx
@@ -5,10 +5,6 @@ import { Secrets, SecretsAlert } from '../SecretsOverview/SecretsAlert';
 import NoAccessAlert from './NoAccessAlert';
 import { ViewSettingsButton } from './ViewSettingsButton';
 import { FilterEnum } from '../../typesFrontend';
-import Alert from '@mui/material/Alert';
-import { Error } from '@mui/icons-material';
-import AlertTitle from '@mui/material/AlertTitle';
-import Link from '@mui/material/Link';
 
 interface StarFilterProps {
   hasStarred: boolean;
@@ -49,37 +45,24 @@ export const PageHeader = ({
   rightActions,
 }: Props) => {
   return (
-    <>
-      <Stack flexDirection="row" alignItems="center" flexWrap="wrap" gap={2}>
-        <MetricsStatus entityName={entityName} />
-        <Stack
-          flexDirection="row"
-          gap={2}
-          flex={1}
-          flexWrap="wrap"
-          sx={{ '& > *': { flex: 1 } }}
-        >
-          <SecretsAlert secretsOverviewData={secrets} />
-        </Stack>
-        <Box display="flex" alignItems="center" flexWrap="wrap" gap={0.5}>
-          <ViewSettingsButton {...viewSettingsProps} />
-          {rightActions}
-        </Box>
+    <Stack flexDirection="row" alignItems="center" flexWrap="wrap" gap={2}>
+      <MetricsStatus entityName={entityName} />
+      <Stack
+        flexDirection="row"
+        gap={2}
+        flex={1}
+        flexWrap="wrap"
+        sx={{ '& > *': { flex: 1 } }}
+      >
+        <SecretsAlert secretsOverviewData={secrets} />
+        {notPermitted && notPermitted.length > 0 && (
+          <NoAccessAlert repos={notPermitted} />
+        )}
       </Stack>
-      {notPermitted && notPermitted.length > 0 && (
-        <NoAccessAlert repos={notPermitted} />
-      )}
-      <Alert severity="error" icon={<Error />}>
-        <AlertTitle fontSize={50}>
-          Tjenesten for å samle inn sårbarheter er for tiden utilgjengelig.
-        </AlertTitle>
-        <AlertTitle fontSize={25} paddingTop={2}>
-          Sikkerhetsmetrikker vil derfor <strong>ikke</strong> gi et riktig
-          oversiktsbilde over sårbarheter. Vi jobber med saken. I mellomtiden er
-          dere nødt til å benytte GitHub Security og{' '}
-          <Link href="https://eu1.app.sysdig.com/">sysdig</Link>
-        </AlertTitle>
-      </Alert>
-    </>
+      <Box display="flex" alignItems="center" flexWrap="wrap" gap={0.5}>
+        <ViewSettingsButton {...viewSettingsProps} />
+        {rightActions}
+      </Box>
+    </Stack>
   );
 };
